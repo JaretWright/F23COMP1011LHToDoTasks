81a6675b5b2de680031e7e6fe6590c1a51688865
JaretWright <Jaret.Wright@GeorgianCollege.ca>
2023-09-14 13:18:27 -0400

    Added validation in the Task class

--- src/main/java/com/example/f23comp1011lhtodotasks/Task.java
+++ src/main/java/com/example/f23comp1011lhtodotasks/Task.java
@@ -1,11 +1,13 @@
 package com.example.f23comp1011lhtodotasks;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.TreeSet;
 
 public class Task {
     private String title, description, category;
     private Person assignedTo;
-    private LocalDate dueDate;
+    private LocalDate creationDate, dueDate;
     private int priority;
     private boolean inProgress, done;
 
@@ -20,12 +22,39 @@ public class Task {
         done = false;
     }
 
+    public Task(String title, String description, String category, Person assignedTo, LocalDate creationDate, LocalDate dueDate, int priority) {
+        this(title,description,category,assignedTo,dueDate,priority);  //calls the other constructor
+        setCreationDate(creationDate);
+    }
+
+    public LocalDate getCreationDate() {
+        return creationDate;
+    }
+
+    private void setCreationDate(LocalDate creationDate) {
+        this.creationDate = creationDate;
+    }
+
     public String getTitle() {
         return title;
     }
 
     public void setTitle(String title) {
-        this.title = title;
+        if (lengthValidator(title, 30))
+            this.title = title;
+        else
+            throw new IllegalArgumentException("title must be 1 to 30 characters");
+    }
+
+    /**
+     * This method will ensure the string has 1 to X number of characters and remove
+     * any leading or trailing white spaces
+     * @return
+     */
+    public boolean lengthValidator(String textToValidate, int maxLengthOfString)
+    {
+        textToValidate = textToValidate.trim();
+        return textToValidate.length()>=1 && textToValidate.length()<=maxLengthOfString;
     }
 
     public String getDescription() {
@@ -33,15 +62,36 @@ public class Task {
     }
 
     public void setDescription(String description) {
-        this.description = description;
+        if (lengthValidator(description, 250))
+            this.description = description;
+        else
+            throw new IllegalArgumentException("description must be 1 to 250 characters");
     }
 
     public String getCategory() {
         return category;
     }
 
+    /**
+     * List -> handle multiple objects, user can order the elements, can support duplicates
+     * Set -> this prevents duplicates, the system sets the order
+     * @return
+     */
+    public static TreeSet<String> getCategories()
+    {
+        TreeSet<String> categories = new TreeSet<>();
+        categories.add("personal");
+        categories.add("work");
+        categories.add("school");
+        return categories;
+    }
+
     public void setCategory(String category) {
-        this.category = category;
+        category = category.trim().toLowerCase();
+        if (getCategories().contains(category))
+            this.category = category;
+        else
+            throw new IllegalArgumentException("category must be one of "+getCategories());
     }
 
     public Person getAssignedTo() {
@@ -56,8 +106,19 @@ public class Task {
         return dueDate;
     }
 
+    /**
+     * new task -> due date can only be today or in the future
+     * existing task -> due date can be anytime
+     * @param dueDate
+     */
     public void setDueDate(LocalDate dueDate) {
-        this.dueDate = dueDate;
+        //test if it is an existing task
+        if (creationDate.isBefore(LocalDate.now()))
+            this.dueDate = dueDate;
+        else if (dueDate.isBefore(LocalDate.now()))
+            throw new IllegalArgumentException("due date cannot be before today");
+        else
+            this.dueDate = dueDate;
     }
 
     public int getPriority() {
