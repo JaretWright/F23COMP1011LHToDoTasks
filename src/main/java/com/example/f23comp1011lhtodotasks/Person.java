7214ebb4e0181c7f46d9019abd9d5db4a177a701
JaretWright <Jaret.Wright@GeorgianCollege.ca>
2023-09-14 12:02:56 -0400

    Added validation for the person.java class

--- src/main/java/com/example/f23comp1011lhtodotasks/Person.java
+++ src/main/java/com/example/f23comp1011lhtodotasks/Person.java
@@ -1,5 +1,34 @@
 package com.example.f23comp1011lhtodotasks;
 
 public class Person {
-    
+    private String fullName, email;
+
+    public Person(String fullName, String email) {
+        setFullName(fullName);
+        setEmail(email);
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        fullName = fullName.trim();
+        if (fullName.matches("[A-z\\s\\-]*"))
+            this.fullName = fullName;
+        else
+            throw new IllegalArgumentException("name can only contain letters, space or -");
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        email = email.trim();
+        if (email.matches("^(.+)@(\\S+)$"))
+            this.email = email;
+        else
+            throw new IllegalArgumentException("enter a valid email address");
+    }
 }
